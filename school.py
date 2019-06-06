ce05131237d75b577a901a71ebfd54d5923d8f32
Parker Lumkes <plumkes@gmail.com>
2019-06-06 08:51:00 -0500

    not finished; coming back

--- school.py
+++ school.py
@@ -1,1 +1,14 @@
 class School:
+    def __init__(self, name=None, roster={}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, stdnt_name, grade):
+        student_name = []
+        self.roster[grade] = student_name.append(stdnt_name)
+
+             
+
+    def grade(self, grd):
+        for keys, values in self.roster.items():
+            return keys
